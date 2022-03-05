e7857565db3af8301c93a32d4920ba74278968c1
EdgarSargsyan2001 <edgar.sargsyan.2001@mail.ru>
2022-03-05 21:54:36 +0400

    erorr

--- src/Square.jsx
+++ src/Square.jsx
@@ -35,13 +35,15 @@ if(!player || alonegame){
     const NUMarr = []
     let flag = true
 
-    let arr = ClickedArray.map((el ,index)=>{
+    let arr = ClickedArray.map((el,index)=>{
         
         if(el === "") return index
         if(el === "X") return el + index
         if(el === "O") return el + index
 
-    }).forEach( el => {
+    })
+    
+    arr.forEach( el => {
         if(el[0] === "X" ){
 
             Xarr.push(+el[1])
@@ -60,13 +62,9 @@ if(!player || alonegame){
     
 for(let i = 0; i < 8; i++){
 
-    if(
-       ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[1]) ) ||
-       ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[2]) ) ||
-       ( winComb[i].includes(Oarr[1]) && winComb[i].includes(Oarr[2]) )
-        
-      ){
-            
+    if(( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[1]) ) || ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[2]) ) || ( winComb[i].includes(Oarr[1]) && winComb[i].includes(Oarr[2]) )){
+       
+       
         let nowOod = winComb[i].
                             filter(( el => (el === Oarr[0] || el === Oarr[1] || el === Oarr[2]) ? false : true ))
     
@@ -84,12 +82,12 @@ for(let i = 0; i < 8; i++){
  if(flag){
     for(let i = 0; i < 8; i++){
     
-        if(
-           ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[1]) ) ||
-           ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[2]) ) ||
-           ( winComb[i].includes(Oarr[1]) && winComb[i].includes(Oarr[2]) )
+        if( ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[1]) ) || ( winComb[i].includes(Oarr[0]) && winComb[i].includes(Oarr[2]) ) || ( winComb[i].includes(Oarr[1]) && winComb[i].includes(Oarr[2]) ) ){
+           
+           
+           
         
-          ){
+          
               
            let nowXod = winComb[i].
                                 filter((el=> (el === Xarr[0] || el === Xarr[1] || el === Xarr[2]) ? false : true ))
